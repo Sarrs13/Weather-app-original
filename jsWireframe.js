83bc1c321b0712ba52a612cec15499a47d7947fb
Sarrs13 <97879616+Sarrs13@users.noreply.github.com>
2022-01-20 21:57:59 +0100

    Fix current location commit

--- jsWireframe.js
+++ jsWireframe.js
@@ -53,7 +53,7 @@ window.onload = function () {
     console.log(position);
     axios
       .get(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=` +
+        `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=` +
           appid
       )
       .then(function (response) {
